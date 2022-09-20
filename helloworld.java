a02922944778d9db64259bad42240c6d9f39fa36
chandu <chandrateja.yejarla@gmail.com>
2022-09-20 07:13:41 +0000

    hello world.java has been modified and added

--- helloworld.java
+++ helloworld.java
@@ -1,7 +1,8 @@
-public class Helloworld {
-
+public class Helloworld{
+	//hello world program
 	public static void main(string args[]){
-		system.out.println("Hello-world");
+		system.out.println("hello-world");
 	}
+
 }
 
